5d6d01769160ddd14630fac6896be35864d10ecd
Yannick Signer <44625151+yannicksigner@users.noreply.github.com>
2021-09-25 21:32:55 +0100

    Update search.js

--- assets/javascript/search.js
+++ assets/javascript/search.js
@@ -1,69 +1,68 @@
 (function() {
 
-    var documents = [
-    {
-        "id": 1,
+  var documents = [{
+      "id": 1,
+      "title": "Wedgwood's First Factory",
+      "thumbnailurl": "static-cse.canva.com/blob/651263/youtube.b1db6241.jpg",
+      "summary": "Everyone knows the name of Wedgwood when it comes to pottery, and this is what they have in mind, this blue and white design, it’s famous throughout the world. But few people know that it was here in Burslem on the outskirts of Stoke-On-Trent, that Josiah Wedgwood’s climb to fame and fortune first began. This is actually the site of his first factory, which was at the heart of the pottery industry when it took off in the eighteenth century, but does any of it remain under the paving stones of present day Burslem.",
+      "location": "Burslem, Stoke-on-Trent",
+      "report": {
+        "availability": "no",
+        "link": "no",
+        "abstract": "no"
+      },
+      "broadcast": {
+        "date": "03.01.1999",
+        "seriesepisode": 1,
+        "series": 6,
+        "episode": 31,
         "title": "Wedgwood's First Factory",
-        "thumbnailurl": "static-cse.canva.com/blob/651263/youtube.b1db6241.jpg",
-        "summary": "Everyone knows the name of Wedgwood when it comes to pottery, and this is what they have in mind, this blue and white design, it’s famous throughout the world. But few people know that it was here in Burslem on the outskirts of Stoke-On-Trent, that Josiah Wedgwood’s climb to fame and fortune first began. This is actually the site of his first factory, which was at the heart of the pottery industry when it took off in the eighteenth century, but does any of it remain under the paving stones of present day Burslem.",
-        "location": "Burslem, Stoke-on-Trent",
-        "report": {
-            "availability": "no",
-            "link": "no",
-            "abstract": "no"
+        "year": 1999
+      },
+      "links": {
+        "her": {
+          "name": "no",
+          "globallink": "no",
+          "url": "no"
         },
-        "broadcast": {
-            "date": "03.01.1999",
-            "seriesepisode": 1,
-            "series": 6,
-            "episode": 31,
-            "title": "Wedgwood's First Factory",
-            "year": 1999
-        },
-        "links": {
-            "her": {
-                "name": "no",
-                "globallink": "no",
-                "url": "no"
-            },
-            "imdb": {
-                "name": "no",
-                "url": "no"
-            }
+        "imdb": {
+          "name": "no",
+          "url": "no"
         }
+      }
     },
     {
-        "id": 2,
+      "id": 2,
+      "title": "Wedgwood's First Factory",
+      "thumbnailurl": "static-cse.canva.com/blob/651263/youtube.b1db6241.jpg",
+      "summary": "Everyone knows the name of Wedgwood when it comes to pottery, and this is what they have in mind, this blue and white design, it’s famous throughout the world. But few people know that it was here in Burslem on the outskirts of Stoke-On-Trent, that Josiah Wedgwood’s climb to fame and fortune first began. This is actually the site of his first factory, which was at the heart of the pottery industry when it took off in the eighteenth century, but does any of it remain under the paving stones of present day Burslem.",
+      "location": "Burslem, Stoke-on-Trent",
+      "report": {
+        "availability": "no",
+        "link": "no",
+        "abstract": "no"
+      },
+      "broadcast": {
+        "date": "03.01.1999",
+        "seriesepisode": 1,
+        "series": 6,
+        "episode": 31,
         "title": "Wedgwood's First Factory",
-        "thumbnailurl": "static-cse.canva.com/blob/651263/youtube.b1db6241.jpg",
-        "summary": "Everyone knows the name of Wedgwood when it comes to pottery, and this is what they have in mind, this blue and white design, it’s famous throughout the world. But few people know that it was here in Burslem on the outskirts of Stoke-On-Trent, that Josiah Wedgwood’s climb to fame and fortune first began. This is actually the site of his first factory, which was at the heart of the pottery industry when it took off in the eighteenth century, but does any of it remain under the paving stones of present day Burslem.",
-        "location": "Burslem, Stoke-on-Trent",
-        "report": {
-            "availability": "no",
-            "link": "no",
-            "abstract": "no"
+        "year": 1999
+      },
+      "links": {
+        "her": {
+          "name": "no",
+          "globallink": "no",
+          "url": "no"
         },
-        "broadcast": {
-            "date": "03.01.1999",
-            "seriesepisode": 1,
-            "series": 6,
-            "episode": 31,
-            "title": "Wedgwood's First Factory",
-            "year": 1999
-        },
-        "links": {
-            "her": {
-                "name": "no",
-                "globallink": "no",
-                "url": "no"
-            },
-            "imdb": {
-                "name": "no",
-                "url": "no"
-            }
+        "imdb": {
+          "name": "no",
+          "url": "no"
         }
+      }
     }
-]
+  ]
 
   function displaySearchResults(results, store) {
     var searchResults = document.getElementById('search-results');
@@ -71,10 +70,12 @@
     if (results.length) { // Are there any results?
       var appendString = '';
 
-      for (var i = 0; i < results.length; i++) {  // Iterate over the results
-        var item = documents.find(({id}) => id === results[i].ref)
-        appendString += '<li><img src="'+ item.thumbnailurl +'" alt="" width="600" height="400">';
-        appendString += '<h3>' + item.title + '</h3><p>'+ item.summary+'</p></li>';
+      for (var i = 0; i < results.length; i++) { // Iterate over the results
+        var item = documents.find(({
+          id
+        }) => id === results[i].ref)
+        appendString += '<li><img src="' + item.thumbnailurl + '" alt="" width="600" height="400">';
+        appendString += '<h3>' + item.name + '</h3><p>' + item.text + '</p></li>';
       }
 
       searchResults.innerHTML = appendString;
@@ -104,15 +105,15 @@
 
     // Initalize lunr with the fields it will be searching on. I've given title
     // a boost of 10 to indicate matches on this field are more important.
-    var idx = lunr(function () {
-  this.ref('id')
-  this.field('title')
-  this.field('location')
+    var idx = lunr(function() {
+      this.ref('id')
+      this.field('title')
+      this.field('location')
 
-  documents.forEach(function (doc) {
-    this.add(doc)
-  }, this)
-})
+      documents.forEach(function(doc) {
+        this.add(doc)
+      }, this)
+    })
 
     var results = idx.search(searchTerm); // Get lunr to perform a search
     displaySearchResults(results, window.store); // We'll write this in the next section
