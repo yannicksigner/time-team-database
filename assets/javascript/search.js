ce929accff1279d2027d9e93c08597a6c6fb1c2c
Yannick Signer <44625151+yannicksigner@users.noreply.github.com>
2021-09-25 21:41:06 +0100

    Update search.js

--- assets/javascript/search.js
+++ assets/javascript/search.js
@@ -71,10 +71,8 @@
       var appendString = '';
 
       for (var i = 0; i < results.length; i++) { // Iterate over the results
-        var item = documents.find(({
-          id
-        }) => id === results[i].ref)
-        appendString += '<li><img src="' + item.thumbnailurl + '" alt="" width="600" height="400">';
+        var item = documents.find(({id}) => id === results[i].ref)
+        appendString += '<li><img src="http://' + item.thumbnailurl + '" alt="" width="600" height="400">';
         appendString += '<h3>' + item.name + '</h3><p>' + item.text + '</p></li>';
       }
 
