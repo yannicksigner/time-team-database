22f62035f03cf635eeb2183363e0b47e3e5baffd
Yannick Signer <44625151+yannicksigner@users.noreply.github.com>
2021-09-25 22:18:24 +0100

    Update search.js

--- assets/javascript/search.js
+++ assets/javascript/search.js
@@ -73,7 +73,7 @@
       for (var i = 0; i < results.length; i++) { // Iterate over the results
         var item = documents.find(({id}) => id === parseInt(results[i].ref))
         appendString += '<section class="search-result-item">'
-        appendString += '<img class="image" src="http://' + item.thumbnailurl + '">'
+        appendString += '<a class="image-link" href="#"><img class="image" src="http://' + item.thumbnailurl + '"></a>'
         appendString += '<div class="search-result-item-body">'
         appendString += '<div class="row"><div class="col-sm-9">'
         appendString += '<h4 class="search-result-item-heading">' + item.title + '</h4>'
