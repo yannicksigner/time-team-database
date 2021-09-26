71865bf4556bb82813359c93183fc6969458fef0
Yannick Signer <44625151+yannicksigner@users.noreply.github.com>
2021-09-26 15:45:33 +0100

    Update search.js

--- assets/javascript/search.js
+++ assets/javascript/search.js
@@ -106,7 +106,7 @@
         if (item.report.availability) {
           appendString += '<p class="value3 mt-sm"><i class="bi bi-file-text-fill" style="margin-right: 15px;" data-toggle="tooltip" data-placement="left" title="Report"></i>' + item.report.title + ' <a href="http://' + item.report.url + '" target="_blank"><i class="bi bi-link-45deg"></i></a></p></div>'
         } else {
-          appendString += '<p class="value3 mt-sm"><i class="bi bi-file-text" style="margin-right: 15px;" data-toggle="tooltip" data-placement="left" title="HER"></i> no excavation report identified</p>'
+          appendString += '<p class="value3 mt-sm"><i class="bi bi-file-text" style="margin-right: 15px;" data-toggle="tooltip" data-placement="left" title="HER"></i> no excavation report identified</p></div>'
         }
 
         appendString += '<div class="col-sm-4 text-align-center">'
