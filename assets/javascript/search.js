2d770e3a9e971436292c0e8c4a43afdac318cfa2
Yannick Signer <44625151+yannicksigner@users.noreply.github.com>
2021-09-27 15:20:37 +0100

    Update search.js

--- assets/javascript/search.js
+++ assets/javascript/search.js
@@ -128,7 +128,7 @@
   if (searchTerm) {
     document.getElementById('search-box').setAttribute("value", searchTerm);
 
-    import Fuse from 'fuse.js'
+    import Fuse from '/sturdy-robots/assets/javascript/fuse.js'
 
     const options = {
       keys: [
