f2cf9125d52b0efdc3d6693b17294d84b359e7b9
Yannick Signer <44625151+yannicksigner@users.noreply.github.com>
2021-10-14 15:24:29 +0100

    Update search.js

--- assets/javascript/search.js
+++ assets/javascript/search.js
@@ -121,11 +121,14 @@
         appendString += '<h4 class="search-result-item-heading">' + item.broadcast.title + ' </h4>'
         //<span class="badge bg-success">'+ ((1-parseFloat(results[i].score)).toFixed(3) * 100).toFixed(1) + '% match</span>
         appendString += '<p class="info">' + item.location + ' (' + item.country + ') at <a href="http://osmaps.ordnancesurvey.co.uk/' + item.coord + ',11/pin" target="_blank">' + item.ngr + '</i></a></p>'
-        appendString += '<p class="description">' + item.summary + '</p>'
 
         if (item.periods.prehistoric != "–"){
           appendString += addPeriodBadges(item);
         }
+
+        appendString += '<p class="description">' + item.summary + '</p>'
+
+
 
         appendString += '<hr/>'
         // SUB-HEADER
